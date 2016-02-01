c7d4ad3b4ffb80918f33ec8d568209e0e0be8f66
Ammar <ammarj965@gmail.com>
2016-02-01 16:29:07 -0500

    Consistant spacing

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,13 +1,13 @@
-def bubble_sort( array ):
+def bubble_sort(array):
     ''' array -> int array
         Sorting by doing adjacent comparisons repeatedly 
         through the array, until everything is sorted '''
 
-    to_be_sorted = len( array ) - 1
+    to_be_sorted = len(array) - 1
 
     while to_be_sorted > 0:
 
-        for i in range( to_be_sorted ):
+        for i in range(to_be_sorted):
             if array[i] > array [i+1]:
                 array[i], array[i+1] = array[i+1], array[i]
 
@@ -20,16 +20,16 @@ if __name__ == "__main__":
     print "Test: "
     print example_one
     print "Output: "
-    print bubble_sort( example_one )
+    print bubble_sort(example_one)
 
     example_two = [12342, 12341] 
     print "Test: "
     print example_two
     print "Output: "
-    print bubble_sort( example_two )
+    print bubble_sort(example_two)
 
     example_three = [1232, 123, 21, 341, 231, 32135, 123, 3, 2, 5, 231, 993, 7, 3, 4, 4, 4]
     print "Test: "
     print example_three
     print "Output: "
-    print bubble_sort( example_three )
+    print bubble_sort(example_three)
--- merge_sort.py
+++ merge_sort.py
@@ -1,4 +1,4 @@
-def merge_sort(array):
+def merge_sort( array ):
     ''' array -> int array
         Recursively call merge_sort with subarrays of the original,
         until all sub arrays are sorted and can be merges using the sort
@@ -6,15 +6,15 @@ def merge_sort(array):
 
     # If the size of the array is 0 or 1, 
     # then it is already sorted
-    if len( array ) < 2:
+    if len(array) < 2:
         return array
 
     # calculate the middle of the array (round down)
-    mid = len( array ) // 2
+    mid = len(array) // 2
 
     # Sort the subarrays
-    left_sub_array = merge_sort( array[0:mid] )
-    right_sub_array = merge_sort( array[mid:] )
+    left_sub_array = merge_sort(array[0:mid])
+    right_sub_array = merge_sort(array[mid:])
 
     # Init variables
     sorted_array = []
@@ -35,10 +35,10 @@ def merge_sort(array):
 
         # Otherwise append the smaller of the two numbers the indices are currently on.
         if left_sub_array[left_index] < right_sub_array[right_index]:
-            sorted_array.append( left_sub_array[left_index] )
+            sorted_array.append(left_sub_array[left_index])
             left_index += 1
         else:
-            sorted_array.append( right_sub_array[right_index] )
+            sorted_array.append(right_sub_array[right_index])
             right_index += 1
 
     return sorted_array
@@ -48,16 +48,16 @@ if __name__ == "__main__":
     print "Test: " 
     print example_one
     print "Output: "
-    print merge_sort( example_one )
+    print merge_sort(example_one)
 
     example_two = [12342]
     print "Test: " 
     print example_two
     print "Output: "
-    print merge_sort( example_two )
+    print merge_sort(example_two)
 
     example_three = [1232, 123, 21, 341, 231, 32135, 123, 3, 2, 5, 231, 993, 7, 3, 4, 4, 4]
     print "Test: "
     print example_three
     print "Output: "
-    print merge_sort( example_three )
+    print merge_sort(example_three)
